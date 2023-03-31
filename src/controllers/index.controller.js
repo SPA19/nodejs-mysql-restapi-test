77a75475964d8ca09a077f2967b1c683a3f67b5a
Simon Posada Acosta SION <simon.150@hotmail.com>
2023-03-31 12:12:41 -0500

    change message and endpoint

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const ping = async (req, res) => {
   const [result] = await pool.query(
-    'SELECT "Estoy en mi base de datooooooos" AS result'
+    'SELECT "Hola Mundo" AS result'
   );
   res.json(result[0]);
 };
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,6 @@ import { ping } from "../controllers/index.controller.js";
 
 const router = Router();
 
-router.get("/ping", ping);
+router.get("/saludo", ping);
 
 export default router;
